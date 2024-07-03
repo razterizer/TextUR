7fe432dca172b9ba1399b5a25278fa125149fe25
Rasmus Anthin <scratty@gmail.com>
2024-07-03 16:25:59 +0200

    textur.cpp: * Added a bunch of textel presets. * Added menu of different textel presets. * Added basic keyboard control of caret and menu items (menu items not hilighted yet). * Improved menu drawing order of different menu parts.

--- TextUR/textur.cpp
+++ TextUR/textur.cpp
@@ -20,6 +20,33 @@ enum class EditorFileMode { NEW_OR_OVERWRITE_FILE, OPEN_EXISTING_FILE };
 
 class Game : public GameEngine<>
 {
+  struct TextelItem
+  {
+    TextelItem(drawing::Textel t, std::string a_name)
+      : textel(std::move(t))
+      , name(std::move(a_name))
+    {}
+    
+    drawing::Textel textel;
+    std::string name;
+  };
+
+  void draw_menu(const styles::Style& ui_style, const int menu_width)
+  {
+    //const int nr = sh.num_rows();
+    const int nc = sh.num_cols();
+  
+    int r = 0;
+    for (const auto& preset : textel_presets)
+    {
+      const auto& textel = preset.textel;
+      sh.write_buffer(textel.str(), r + 1, nc - menu_width + 2, textel.get_style());
+      sh.write_buffer(preset.name, r + 2, nc - menu_width + 2, ui_style);
+      draw_box_outline(sh, r, nc - menu_width, 4 - 1, menu_width - 1, drawing::OutlineType::Line, ui_style);
+      r += 3;
+    }
+  }
+
 public:
   Game(int argc, char** argv, const GameEngineParams& params)
     : GameEngine(argv[0], params)
@@ -60,6 +87,39 @@ public:
     cs.side_h.bg_color = Color::DarkBlue;
     
     font_data = ASCII_Fonts::load_font_data(font_data_path);
+    
+    textel_presets.emplace_back(drawing::Textel { '_', Color::Default, Color::LightGray, 0 }, "Tile");
+    textel_presets.emplace_back(drawing::Textel { '~', Color::DarkBlue, Color::Blue, 1 }, "Water0");
+    textel_presets.emplace_back(drawing::Textel { '*', Color::White, Color::Blue, 1 }, "Water1");
+    textel_presets.emplace_back(drawing::Textel { ':', Color::DarkYellow, Color::Yellow, 2 }, "Sand0");
+    textel_presets.emplace_back(drawing::Textel { '.', Color::DarkYellow, Color::Yellow, 2 }, "Sand1");
+    textel_presets.emplace_back(drawing::Textel { '8', Color::DarkGray, Color::LightGray, 3 }, "Stone0");
+    textel_presets.emplace_back(drawing::Textel { 'o', Color::DarkGray, Color::LightGray, 3 }, "Stone1");
+    textel_presets.emplace_back(drawing::Textel { 'H', Color::DarkGray, Color::LightGray, 4 }, "Masonry");
+    textel_presets.emplace_back(drawing::Textel { '#', Color::DarkRed, Color::Red, 5 }, "Brick");
+    textel_presets.emplace_back(drawing::Textel { '|', Color::DarkGreen, Color::Green, 6 }, "Grass0");
+    textel_presets.emplace_back(drawing::Textel { '.', Color::DarkGreen, Color::Green, 6 }, "Grass1");
+    textel_presets.emplace_back(drawing::Textel { ':', Color::DarkGreen, Color::Green, 6 }, "Grass2");
+    textel_presets.emplace_back(drawing::Textel { '/', Color::DarkGreen, Color::Green, 6 }, "Grass3");
+    textel_presets.emplace_back(drawing::Textel { '\\', Color::DarkGreen, Color::Green, 6 }, "Grass4");
+    textel_presets.emplace_back(drawing::Textel { '&', Color::DarkGreen, Color::Green, 7 }, "Shrub0");
+    textel_presets.emplace_back(drawing::Textel { '@', Color::DarkGreen, Color::Green, 7 }, "Shrub1");
+    textel_presets.emplace_back(drawing::Textel { '*', Color::DarkGreen, Color::Green, 7 }, "Shrub2");
+    textel_presets.emplace_back(drawing::Textel { 'T', Color::DarkGreen, Color::Green, 8 }, "Tree");
+    textel_presets.emplace_back(drawing::Textel { '=', Color::DarkGray, Color::LightGray, 9 }, "Metal");
+    textel_presets.emplace_back(drawing::Textel { 'W', Color::DarkRed, Color::Yellow, 10 }, "Wood0");
+    textel_presets.emplace_back(drawing::Textel { 'E', Color::DarkRed, Color::Yellow, 10 }, "Wood1");
+    textel_presets.emplace_back(drawing::Textel { 'Z', Color::DarkRed, Color::Yellow, 10 }, "Wood2");
+    textel_presets.emplace_back(drawing::Textel { 'X', Color::DarkBlue, Color::Cyan, 11 }, "Ice");
+    textel_presets.emplace_back(drawing::Textel { '^', Color::DarkGray, Color::LightGray, 12 }, "Mountain");
+    textel_presets.emplace_back(drawing::Textel { ' ', Color::Default, Color::Black, 13 }, "Void");
+    textel_presets.emplace_back(drawing::Textel { 'W', Color::DarkRed, Color::Red, 14 }, "Lava");
+    textel_presets.emplace_back(drawing::Textel { 'C', Color::DarkYellow, Color::Yellow, 15 }, "Cave");
+    textel_presets.emplace_back(drawing::Textel { 'S', Color::DarkGreen, Color::Green, 16 }, "Swamp");
+    textel_presets.emplace_back(drawing::Textel { 'P', Color::DarkYellow, Color::Yellow, 17 }, "Path");
+    textel_presets.emplace_back(drawing::Textel { 'M', Color::DarkGray, Color::LightGray, 18 }, "Mine");
+    textel_presets.emplace_back(drawing::Textel { 'G', Color::DarkYellow, Color::Yellow, 19 }, "Gold");
+
   }
   
 private:
@@ -69,10 +129,17 @@ private:
     
     styles::Style ui_style { Color::LightGray, Color::Black };
     
+    const int nr = sh.num_rows();
+    const int nc = sh.num_cols();
+    const int menu_width = 12;
+      
     if (show_menu)
-      draw_box_outline(sh, 0, sh.num_cols() - 10, sh.num_rows() - 1, 10 - 1, drawing::OutlineType::Line, ui_style);
+      draw_box_outline(sh, 0, nc - menu_width, nr - 1, menu_width - 1, drawing::OutlineType::Line, ui_style);
   
     draw_frame(sh, Color::White);
+    
+    if (show_menu)
+      draw_menu(ui_style, menu_width);
     
     // Caret
     if (anim_ctr % 2 == 0)
@@ -91,6 +158,53 @@ private:
     // Keypresses:
     if (kpd.curr_key == ' ')
       math::toggle(show_menu);
+      
+    bool is_up = kpd.curr_special_key == keyboard::SpecialKey::Up || str::to_lower(kpd.curr_key) == 'w';
+    bool is_down = kpd.curr_special_key == keyboard::SpecialKey::Down || str::to_lower(kpd.curr_key) == 's';
+    bool is_left = kpd.curr_special_key == keyboard::SpecialKey::Left || str::to_lower(kpd.curr_key) == 'a';
+    bool is_right = kpd.curr_special_key == keyboard::SpecialKey::Right || str::to_lower(kpd.curr_key) == 'd';
+    if (show_menu)
+    {
+      if (is_up)
+      {
+        selected_textel_preset_idx--;
+        if (selected_textel_preset_idx == -1)
+          selected_textel_preset_idx = static_cast<int>(textel_presets.size()) - 1;
+      }
+      else if (is_down)
+      {
+        selected_textel_preset_idx++;
+        if (selected_textel_preset_idx == static_cast<int>(textel_presets.size()))
+          selected_textel_preset_idx = 0;
+      }
+    }
+    else
+    {
+      if (is_up)
+      {
+        caret_pos.r--;
+        if (caret_pos.r < 0)
+          caret_pos.r = 0;
+      }
+      else if (is_down)
+      {
+        caret_pos.r++;
+        if (caret_pos.r >= static_cast<int>(curr_texture.size.r))
+          caret_pos.r = curr_texture.size.r - 1;
+      }
+      else if (is_left)
+      {
+        caret_pos.c--;
+        if (caret_pos.c < 0)
+          caret_pos.c = 0;
+      }
+      else if (is_right)
+      {
+        caret_pos.c++;
+        if (caret_pos.c >= static_cast<int>(curr_texture.size.c))
+          caret_pos.c = curr_texture.size.c - 1;
+      }
+    }
   }
   
   virtual void draw_title() override
@@ -114,9 +228,13 @@ private:
   EditorFileMode file_mode = EditorFileMode::OPEN_EXISTING_FILE;
   
   RC screen_pos { 0, 0 };
+  //ttl::Rectangle m_screen_in_world;
   RC caret_pos { 0, 0 };
   
   bool show_menu = false;
+  
+  std::vector<TextelItem> textel_presets;
+  int selected_textel_preset_idx = 0;
 };
 
 int main(int argc, char** argv)
