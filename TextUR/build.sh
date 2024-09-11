d76285feef2eb82801ac2d4d3d4d0bb431156a2a
Rasmus Anthin <32767250+razterizer@users.noreply.github.com>
2024-09-11 21:24:08 +0200

    Update build.sh
    Propagating the return code from the base build script in Core.git.

--- TextUR/build.sh
+++ TextUR/build.sh
@@ -11,6 +11,14 @@ fi
 
 ../../lib/Core/build.sh textur "$1" "${additional_flags[@]}"
 
+# Capture the exit code of Core/build.sh
+exit_code=$?
+
+if [ $exit_code -ne 0 ]; then
+  echo "Core/build.sh failed with exit code $exit_code"
+  exit $exit_code
+fi
+
 ### Post-Build Actions ###
 
 mkdir -p bin/fonts/
