80e3021a2c50df50533ae8946b57a84936e49753
Rasmus Anthin <scratty@gmail.com>
2024-12-10 19:09:08 +0100

    fetch-dependencies.py: * Updating script so that it supports the extra optional os column.

--- fetch-dependencies.py
+++ fetch-dependencies.py
@@ -85,6 +85,7 @@ class Dependency:
     path: Path
     repo: str
     ref: str
+    os: str
 
 
 class CheckoutStatus(NamedTuple):
@@ -99,8 +100,9 @@ def parse_dependencies(f) -> Iterator[Dependency]:
         match d:
             case [] | ['#', *_]:
                 pass
-            case [path, repo, ref]:
-                yield Dependency(Path(path), repo, ref)
+            case [path, repo, ref, *os]:
+                os = os[0] if os else "any"
+                yield Dependency(Path(path), repo, ref, os)
             case _:
                 print(f'Error in dependencies file. I do not understand line {i}.', file=sys.stderr)
                 sys.exit(1)
@@ -155,6 +157,18 @@ if __name__ == '__main__':
     error = []
 
     for d in deps:
+        skip = False;
+        # os = 'nt' | 'posix' | 'any'
+        match d.os:
+            case 'win':
+                if not os.name == 'nt':
+                    skip = True;
+            case 'posix':
+                if not os.name == 'posix':
+                    skip = True;
+        if skip:
+          continue;
+    
         dep_wc = (projects_dir / d.path).absolute()
 
         # If the dependency working copy does not exist yet, we can just clone it.
@@ -265,3 +279,4 @@ if __name__ == '__main__':
 #
 #                                  (in case you did not notice)
 ####################################################################################################
+
