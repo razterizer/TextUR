5f1ef33352cb0877f3f3558deb719bb98795b5a0
Rasmus Anthin <scratty@gmail.com>
2025-09-12 17:30:40 +0200

    Hopefully fixes the bash build script so that the workflows work again.

--- TextUR/build.sh
+++ TextUR/build.sh
@@ -3,9 +3,11 @@
 os_name=$(uname)
 
 if [[ $os_name == *"Darwin"* ]]; then
-  additional_flags="-I../../lib -I../../lib/Termin8or/include"
+  additional_flags="-I../../lib \
+    -I../../lib/Termin8or/include"
 else
-  additional_flags="-I../../lib"
+  additional_flags="-I../../lib \
+    -I../../lib/Termin8or/include"
   # export BUILD_PKG_CONFIG_MODULES='openal'
 fi
 
