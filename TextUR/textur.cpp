d6c7bea6afe0278af0fba3b1b6527cbef7f4e5ad
Rasmus Anthin <scratty@gmail.com>
2026-03-25 02:47:59 +0100

    Must check either character text field or glyph picker depending on if ascii_only_textures is set when checking for character/glyph availability when entering a new custom textel (normal + shadow).

--- TextUR/textur.cpp
+++ TextUR/textur.cpp
@@ -1427,7 +1427,7 @@ private:
                                              "You must enter a textel preset name.",
                                              t8x::MessageHandlerLevel::Guide);
               }
-              else if (dialog_editor.text_field_empty(1))
+              else if ((ascii_only_textures && dialog_editor.text_field_empty(1)) || (!ascii_only_textures && dialog_editor.glyph_picker_empty(1)))
               {
                 message_handler->add_message(static_cast<float>(get_real_time_s()),
                                              "You must enter a textel character.",
@@ -1489,7 +1489,7 @@ private:
                                              "You must enter a textel preset name.",
                                              t8x::MessageHandlerLevel::Guide);
               }
-              else if (dialog_editor.text_field_empty(1))
+              else if ((ascii_only_textures && dialog_editor.text_field_empty(1)) || (!ascii_only_textures && dialog_editor.glyph_picker_empty(1)))
               {
                 message_handler->add_message(static_cast<float>(get_real_time_s()),
                                              "You must enter a textel character.",
